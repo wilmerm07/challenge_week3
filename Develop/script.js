e5ab37d7dfdd1be09d6457f9ba751fc68c23f10e
wilmerm07 <128619778+wilmerm07@users.noreply.github.com>
2023-04-19 21:35:25 -0400

    alert added

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,36 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(SpecialCharacters, UpperCaseLetters, LowerCaseLetters, Numbers) {
-  var passwordLength = 18
-  var generatedPassword="";
+function generatePassword() {
+  var generatedPassword = "";
 
-// Do your thing here and generate a passsword
-//  Ask the user how many characters
-var numberOfCharacters=prompt("How many Characters in the password?");
-//  Ask if the user wants to include special characters
-var includeSpecialCharacters=Confirm("Do you want to include Special Characters?");
-var includeUpperCaseLetters=Confirm("Do you want to include Upper Case Letters?");
-var includeLowerCaseLetters=Confirm("Do you want to include Lower Case Letters");
-var includeNumbers=Confirm("Do you want to include Numbers?");
-// Generate a password with the number of characters and the types of characrters selected by user, and put it in the generatedPassword variable
+  // Ask the user how many characters
+  var numberOfCharacters = prompt("How many characters in the password (between 8 and 128)?");
 
+  // Validate the input for the number of characters
+  while (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    numberOfCharacters = prompt("Invalid input. Please enter a number between 8 and 128:");
+  }
 
+  // Ask if the user wants to include special characters
+  var includeSpecialCharacters = confirm("Do you want to include special characters?");
 
-  return password;
+  var includeUpperCaseLetters = confirm("Do you want to include upper case letters?");
+
+  var includeLowerCaseLetters = confirm("Do you want to include lower case letters?");
+
+  var includeNumbers = confirm("Do you want to include numbers?");
+
+  // Validate that at least one type of character is selected
+  while (!includeSpecialCharacters && !includeUpperCaseLetters && !includeLowerCaseLetters && !includeNumbers) {
+    alert("Please select at least one type of character.");
+    includeSpecialCharacters = confirm("Do you want to include special characters?");
+    includeUpperCaseLetters = confirm("Do you want to include upper case letters?");
+    includeLowerCaseLetters = confirm("Do you want to include lower case letters?");
+    includeNumbers = confirm("Do you want to include numbers?");
+  }
+
+  
+  return generatedPassword;
 }
 
 // Write password to the #password input
@@ -26,7 +39,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
